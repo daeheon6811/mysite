42bf0408f6ccb0b646a2e8e4d6090c88f169b741
wow1186 <WOW1186@NAVER.COM>
2021-06-01 10:59:56 +0900

    Paging 수정 완료!

--- mysite02/src/main/java/com/douzone/mysite/web/board/ListAction.java
+++ mysite02/src/main/java/com/douzone/mysite/web/board/ListAction.java
@@ -17,39 +17,51 @@ public class ListAction implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		int cureentPage = 1;
-		int first  = 0;
+		int cureentPage = 0;
+		int first = 0;
 		int second = 0;
+		int blockStartNum=0;
+		int blockLastNum =0;
+		int lastPageNum=0;
 		int pagesize = Paging.getPagecount();
 
 		Paging paging = new Paging();
-		paging.makeBlock(pagesize);
-		paging.makeLastPageNum();
-
-		Integer blockStartNum = paging.getBlockStartNum(); // 그룹 번호
-		Integer blockLastNum = paging.getBlockLastNum();
-		Integer lastPageNum = paging.getLastPageNum();
-
-		request.setAttribute("blockStartNum", blockStartNum);
-		request.setAttribute("blockLastNum", blockLastNum);
-		request.setAttribute("lastPageNum", lastPageNum); // lastPageNum = 6일 때, 7, 8, 9, 10는 링크를 활성화 하지 못함
+		
+		
 
 		String cp = request.getParameter("page");
 
+		/* 최초 홈페이지 입장*/
 		if (cp == null || "null".equals(cp)) {
-		} else {
-			cureentPage = Integer.parseInt(request.getParameter("page"));
+			paging.makeBlock(cureentPage);
+			paging.makeLastPageNum();
+			 blockStartNum = paging.getBlockStartNum(); // 그룹 번호
+			 blockLastNum = paging.getBlockLastNum();
+			 lastPageNum = paging.getLastPageNum();
+			request.setAttribute("curPageNum", cureentPage);
+		} else { /* 페이징 버튼을 눌렸을때*/
 			
+			cureentPage = Integer.parseInt(request.getParameter("page"));
+			paging.makeBlock(cureentPage);
+			paging.makeLastPageNum();
+			 blockStartNum = paging.getBlockStartNum(); // 그룹 번호
+			 blockLastNum = paging.getBlockLastNum();
+			 lastPageNum = paging.getLastPageNum();
 			request.setAttribute("curPageNum", cureentPage);
 		}
+		request.setAttribute("blockStartNum", blockStartNum);
+		request.setAttribute("blockLastNum", blockLastNum);
+		request.setAttribute("lastPageNum", lastPageNum); // lastPageNum = 6일 때, 7, 8, 9, 10는 링크를 활성화 하지 못함
+		
+		
 
 		if (cureentPage != 0) {
-			 first = (cureentPage * pagesize) - pagesize;
+			first = (cureentPage * pagesize) - pagesize;
 		} else {
-			 first = (cureentPage * pagesize);
+			first = (cureentPage * pagesize);
 		}
 		second = (cureentPage * pagesize) + (pagesize - 1);
-	
+
 		List<BoardVo> list = new BoardRepository().findAll(first, pagesize);
 
 		request.setAttribute("list", list);
