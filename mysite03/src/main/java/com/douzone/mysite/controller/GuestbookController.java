e9db191a0a3788974937e1a46500e5ab37176599
wow1186 <WOW1186@NAVER.COM>
2021-06-03 16:58:27 +0900

    mysite03 #02

---
+++ mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -0,0 +1,28 @@
+package com.douzone.mysite.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.douzone.mysite.service.GuestBookService;
+import com.douzone.mysite.vo.GuestBookVo;
+
+@Controller
+@RequestMapping("/guestbook")
+public class GuestbookController {
+
+	@Autowired
+	GuestBookService guestBookService;
+
+	@RequestMapping("")
+	public String index(Model model) {
+
+		List<GuestBookVo> list = guestBookService.getMessageList();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/guestbook/index.jsp";
+	}
+
+}
---
+++ mysite03/src/main/java/com/douzone/mysite/service/GuestBookService.java
@@ -0,0 +1,29 @@
+package com.douzone.mysite.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douzone.mysite.repository.GuestBookRepository;
+import com.douzone.mysite.vo.GuestBookVo;
+
+@Service
+public class GuestBookService {
+
+	@Autowired
+	GuestBookRepository guestBookRepository;
+
+	public List<GuestBookVo> getMessageList() {
+		return guestBookRepository.findAll();
+	}
+
+	public void deleteMessage(Long no, String password) {
+
+	}
+
+	public void addMessage(GuestBookVo vo) {
+
+	}
+
+}
